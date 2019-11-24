31ee9ca16775ccbd704de2cc0364587686a4327d
amjadamju <amjadpanjeeri@gmail.com>
2019-11-25 00:52:10 +0530

    modified gallery

---
+++ gallery.js
@@ -0,0 +1,34 @@
+function operation1()
+{
+    sukrita='<div class="row m-5"><div class="column"><img src="images/sukrita.jpg" style="width:100%"><img src="images/sukrita3.jpg" style="width:100%"><img src="images/sukrita8.jpg" style="width:100%"></div><div class="column"><img src="images/sukrita2.jpg" style="width:100%"><img src="images/sukrita5.jpg" style="width:100%"><img src="images/sukrita9.jpg" style="width:100%"></div><div class="column">    <img src="images/sukrita4.jpg" style="width:100%">    <img src="images/sukrita1.jpg" style="width:100%">    <img src="images/sukrita10.jpg" style="width:100%"></div><div class="column">    <img src="images/sukrita6.jpg" style="width:100%">    <img src="images/sukrita7.jpg" style="width:100%"></div></div> '
+    document.getElementById('img-gallery').innerHTML=sukrita;
+}
+function operation2()
+{
+    aardra='<div class="row m-5"><div class="column">    <img src="images/ardra1.png" style="width:100%"></div><div class="column">    <img src="images/ardra2.png" style="width:100%"></div><div class="column">    <img src="images/ardra3.png" style="width:100%"></div><div class="column">    <img src="images/aardra4.jpg" style="width:100%"></div>'
+    document.getElementById('img-gallery').innerHTML=aardra;
+}
+function operation3()
+{
+    lovelocks='<div class="row m-5"><div class="column">    <img src="images/lovelocks1.jpg" style="width:100%"><img src="images/lovelocks4.jpg" style="width:100%"></div><div class="column">    <img src="images/lovelocks2.jpg" style="width:100%"></div><div class="column">    <img src="images/lovelocks3.jpg" style="width:100%"></div><div class="column">    <img src="images/lovelocks.jpg" style="width:100%"></div>'
+    document.getElementById('img-gallery').innerHTML=lovelocks;
+}
+function operation4()
+{
+    firstlove='<div class="row m-5"><div class="column">    <img src="images/fl6.jpg" style="width:100%">    <img src="images/fl2.png" style="width:100%"></div><div class="column">    <img src="images/fl5.jpg" style="width:100%">    <img src="images/fl1.png" style="width:100%"></div><div class="column">    <img src="images/fl4.png" style="width:100%"></div><div class="column">    <img src="images/fl3.png" style="width:100%"></div>'
+    document.getElementById('img-gallery').innerHTML=firstlove;
+}
+function operation5(){
+    onam='<div class="row m-5"><div class="column">    <img src="images/onam1.jpg" style="width:100%"></div><div class="column">    <img src="images/onam2.jpg" style="width:100%"> </div><div class="column">    <img src="images/onam3.jpg" style="width:100%"></div><div class="column">    <img src="images/onam4.jpg" style="width:100%"></div>'
+    document.getElementById('img-gallery').innerHTML=onam;
+}
+function operation6()
+{
+    bts='<div class="row m-5"><div class="column">       <img src="images/bts5.jpg" style="width:100%"></div><div class="column">    <img src="images/bts6.jpg" style="width:100%"></div><div class="column">    <img src="images/bts3.jpg" style="width:100%"><img src="images/bts1.jpg" style="width:100%"></div> <div class="column">    <img src="images/bts4.jpg" style="width:100%"> <img src="images/backtoschool.jpg" style="width:100%"></div>'
+    document.getElementById('img-gallery').innerHTML=bts;
+}
+function operation7()
+{
+    hridyam='<div class="row m-5"><div class="column">    <img src="images/hridyam1.jpg" style="width:100%"></div><div class="column">    <img src="images/hridyam2.jpg" style="width:100%"></div><div class="column"><img src="images/hridyam3.jpg" style="width:100%"></div><div class="column">   <img src="images/carousel2.jpg" style="width:100%"></div>'
+    document.getElementById('img-gallery').innerHTML=hridyam;
+}
